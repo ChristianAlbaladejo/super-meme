f4e7d8a9dad71fa66857f5c21af082417debf15f
ChristianAlbaladejo <christian.albaladejo06@gmail.com>
2021-01-18 18:31:36 +0100

    api v1.0.1

--- database.js
+++ database.js
@@ -1,29 +1,71 @@
 var sqlite3 = require('sqlite3').verbose()
 var md5 = require('md5')
 
-const DBSOURCE = "db.sqlite" 
+const DBSOURCE = "db.sqlite"
 
 
 let db = new sqlite3.Database(DBSOURCE, (err) => {
     if (err) {
-      // Cannot open database
-      console.error(err.message)
-      throw err
-    }else{
+        // Cannot open database
+        console.error(err.message)
+        throw err
+    } else {
         console.log('Connected to the SQlite database.')
         db.run(`CREATE TABLE phone (
             phone int
-            )`,(err) => {
-        if (err) {
-            // Table already created
-        }else{
-            // Table just created, creating some rows
-            var insert = 'INSERT INTO phone (phone) VALUES (?)'
-            db.run(insert, ["603421431"])
-            db.run(insert, ["655987070"])
-            db.run(insert, ["618740740"])
-        }
-    })  
+            )`, (err) => {
+            if (err) {
+                // Table already created
+            } else {
+                // Table just created, creating some rows
+                var insert = 'INSERT INTO phone (phone) VALUES (?)'
+                db.run(insert, ["603421431"])
+                db.run(insert, ["655987070"])
+                db.run(insert, ["618740740"])
+            }
+        })
+        db.run(`CREATE TABLE phoneAction (
+            acceptPhoneActionType text,
+            acceptId int,
+            errorPhoneActionType text,
+            errorId int
+            )`, (err) => {
+            if (err) {
+                // Table already created
+            } else {
+                // Table just created, creating some rows
+                var insert = 'INSERT INTO phoneAction (acceptPhoneActionType, acceptId,errorPhoneActionType, errorId) VALUES (?,?,?,?)'
+                db.run(insert, ["QUEUE", "6", "IVR", "3"])
+            }
+        })
+        db.run(`CREATE TABLE log (
+            action text,
+            phone text,
+            status text,
+            time text
+            )`, (err) => {
+            if (err) {
+                // Table already created
+            } else {
+                // Table just created, creating some rows
+            }
+        })
+        db.run(`CREATE TABLE user (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name text, 
+            email text UNIQUE, 
+            password text, 
+            CONSTRAINT email_unique UNIQUE (email)
+            )`, (err) => {
+            if (err) {
+                // Table already created
+            } else {
+                // Table just created, creating some rows
+                var insert = 'INSERT INTO user (name, email, password) VALUES (?,?,?)'
+                db.run(insert, ["admin", "admin@example.com", "admin123456"])
+                db.run(insert, ["user", "user@example.com", "user123456"])
+            }
+        })  
     }
 })
 
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var express = require("express")
 var app = express()
 var db = require("./database.js")
-var md5 = require("md5")
+var md_auth = require('./middlewares/authentificated')
+var jwt = require('./services/jwt');
+
 
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -9,16 +11,24 @@ app.use(bodyParser.json());
 
 var HTTP_PORT = 8000
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE, PATCH');
+
+    next();
+});
+
 // Start server
 app.listen(HTTP_PORT, () => {
     console.log("Server running on port %PORT%".replace("%PORT%", HTTP_PORT))
 });
 
-app.get("/api/users", (req, res, next) => {
-    var sql = "select * from phone where phone = 603421431"
+app.get("/api/phoneAction/", md_auth.ensureAuth, (req, res, next) => {
+    var sql = "select * from phoneAction"
     var params = []
     db.all(sql, params, (err, rows) => {
-        console.log(rows)
         if (err) {
             res.status(400).json({ "error": err.message });
             return;
@@ -30,8 +40,52 @@ app.get("/api/users", (req, res, next) => {
     });
 });
 
+app.get("/api/log/", md_auth.ensureAuth, (req, res, next) => {
+    var sql = "select * from log order by time DESC"
+    var params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(400).json({ "error": err.message });
+            return;
+        }
+        res.json({
+            "message": "success",
+            "data": rows
+        })
+    });
+});
 
-app.get("/api/user/:id", (req, res, next) => {
+app.get("/api/phones/", md_auth.ensureAuth, (req, res, next) => {
+    var sql = "select * from phone"
+    var params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(400).json({ "error": err.message });
+            return;
+        }
+        res.json({
+            "message": "success",
+            "data": rows
+        })
+    });
+});
+
+app.get("/api/users", md_auth.ensureAuth, (req, res, next) => {
+    var sql = "select * from user"
+    var params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(400).json({ "error": err.message });
+            return;
+        }
+        res.json({
+            "message": "success",
+            "data": rows
+        })
+    });
+});
+
+app.get("/api/user/:id", md_auth.ensureAuth, (req, res, next) => {
     var sql = "select * from user where id = ?"
     var params = [req.params.id]
     db.get(sql, params, (err, row) => {
@@ -47,20 +101,118 @@ app.get("/api/user/:id", (req, res, next) => {
 });
 
 
-app.post("/api/phone/", (req, res, next) => {
-    /* var errors=[]
-    if (!req.body.password){
-        errors.push("No password specified");
-    }
-    if (!req.body.email){
-        errors.push("No email specified");
-    }
-    if (errors.length){
-        res.status(400).json({"error":errors.join(",")});
-        return;
-    } */
+app.post("/api/phone/", md_auth.ensureAuth, (req, res, next) => {
     var data = {
         phone: req.body.did,
+    }
+    var sql = "select * from phoneAction"
+    var params = []
+    var phoneAction;
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(400).json({ "error": err.message });
+            return;
+        }
+        phoneAction = rows;
+    });
+    console.log(phoneAction)
+    var sql = "select * from phone where phone =" + data.phone
+    var params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(400).json({ "error": err.message });
+            return;
+        }
+        if (rows.length == 0) {
+            res.json({
+                "phoneActionType": phoneAction.errorPhoneActionType,
+                "phoneActionId": phoneAction.errorId,
+            })
+        } else {
+            res.json({
+                "phoneActionType": phoneAction.acceptPhoneActionType,
+                "phoneActionId": phoneAction.errorId,
+            })
+        }
+    });
+})
+
+app.post("/api/user/", md_auth.ensureAuth, (req, res, next) => {
+    var errors = []
+    if (!req.body.password) {
+        errors.push("No password specified");
+    }
+    if (!req.body.email) {
+        errors.push("No email specified");
+    }
+    if (errors.length) {
+        res.status(400).json({ "error": errors.join(",") });
+        return;
+    }
+    var data = {
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    }
+    var sql = 'INSERT INTO user (name, email, password) VALUES (?,?,?)'
+    var params = [data.name, data.email, data.password]
+    db.run(sql, params, function (err, result) {
+        if (err) {
+            res.status(400).json({ "error": err.message })
+            return;
+        }
+        res.json({
+            "message": "success",
+            "data": data,
+            "id": this.lastID
+        })
+    });
+})
+
+app.post("/api/log/", md_auth.ensureAuth, (req, res, next) => {
+    var errors = []
+    if (!req.body.action) {
+        errors.push("No action specified");
+    }
+    if (!req.body.status) {
+        errors.push("No status specified");
+    }
+    if (errors.length) {
+        res.status(400).json({ "error": errors.join(",") });
+        return;
+    }
+    var datetime = new Date();
+    var data = {
+        action: req.body.action,
+        phone: req.body.phone,
+        status: req.body.status,
+        time: datetime.toISOString()
+    }
+    var sql = 'INSERT INTO log (action, phone, status, time) VALUES (?,?,?,?)'
+    var params = [data.action, data.phone, data.status, data.time]
+    db.run(sql, params, function (err, result) {
+        if (err) {
+            res.status(400).json({ "error": err.message })
+            return;
+        }
+        res.json({
+            "message": "success",
+            "data": data
+        })
+    });
+})
+
+app.post("/api/addPhone/", md_auth.ensureAuth, (req, res, next) => {
+    var errors = []
+    if (!req.body.phone) {
+        errors.push("No phone specified");
+    }
+    if (errors.length) {
+        res.status(400).json({ "error": errors.join(",") });
+        return;
+    }
+    var data = {
+        phone: req.body.phone,
     }
     var sql = "select * from phone where phone =" + data.phone
     var params = []
@@ -71,46 +223,51 @@ app.post("/api/phone/", (req, res, next) => {
             return;
         }
         if (rows.length == 0) {
-            res.json({
-                "phoneActionType": "IVR",
-                "phoneActionId": "3",
-            })
+            var sql = 'INSERT INTO phone (phone) VALUES (?)'
+            var params = [data.phone]
+            db.run(sql, params, function (err, result) {
+                if (err) {
+                    var sql = 'INSERT INTO log (action, phone, status, time) VALUES (?,?,?,?)'
+                    var datetime = new Date();
+                    var params = ['Teléfeno añadido', data.phone, "error", datetime.toISOString()]
+                    db.run(sql, params, function (err, result) {
+                        if (err) {
+                            res.status(400).json({ "error": err.message })
+                            return;
+                        }
+                    });
+                    res.status(400).json({ "error": err.message })
+                    return;
+                }
+                var sql = 'INSERT INTO log (action, phone, status, time) VALUES (?,?,?,?)'
+                var datetime = new Date();
+                var params = ['Teléfeno añadido', data.phone, "success", datetime.toISOString()]
+                db.run(sql, params, function (err, result) {
+                    if (err) {
+                        res.status(400).json({ "error": err.message })
+                        return;
+                    }
+                });
+                res.json({
+                    "message": "success",
+                    "data": data,
+                })
+            });
         } else {
             res.json({
-                "phoneActionType": "QUEUE",
-                "phoneActionId": "6",
+                "error": "Ya existe"
             })
         }
     });
-   /*  var sql = 'select * from phone'
-    var params = []
-    db.run(sql,params ,function (err, result) {
-        if (err) {
-            res.status(400).json({ "error": err.message })
-            return;
-        }
-        console.log(result)
-        if (result != undefined) {
-            res.json({
-                "phoneActionType": "IVR",
-                "phoneActionId": "2",
-            })
-        } else {
-            res.json({
-                "phoneActionType": "QUEUE",
-                "phoneActionId": "3",
-            })
-        }
-    }); */
 })
 
 
 
-app.patch("/api/user/:id", (req, res, next) => {
+app.patch("/api/user/:id", md_auth.ensureAuth, (req, res, next) => {
     var data = {
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password ? md5(req.body.password) : undefined
+        password: req.body.password ? req.body.password : undefined
     }
     db.run(
         `UPDATE user set 
@@ -131,18 +288,122 @@ app.patch("/api/user/:id", (req, res, next) => {
         });
 })
 
-
-app.delete("/api/user/:id", (req, res, next) => {
+app.put("/api/phoneAction", md_auth.ensureAuth, (req, res, next) => {
+    var data = {
+        acceptPhoneActionType: req.body.acceptPhoneActionType,
+        acceptId: req.body.acceptId,
+        errorPhoneActionType: req.body.errorPhoneActionType,
+        errorId: req.body.errorId,
+    }
     db.run(
-        'DELETE FROM user WHERE id = ?',
-        req.params.id,
+        `UPDATE phoneAction set 
+           acceptPhoneActionType = coalesce(?,acceptPhoneActionType), 
+           acceptId = COALESCE(?,acceptId), 
+           errorPhoneActionType = coalesce(?,errorPhoneActionType),
+           errorId = COALESCE(?,errorId)
+           `,
+        [data.acceptPhoneActionType, data.acceptId, data.errorPhoneActionType, data.errorId],
+        (err, result) => {
+            if (err) {
+                res.status(400).json({ "error": res.message })
+                return;
+            }
+            var sql = 'INSERT INTO log (action, phone, status, time) VALUES (?,?,?,?)'
+            var datetime = new Date();
+            var params = ['Teléfeno añadido', data.phone, "success", datetime.toISOString()]
+            db.run(sql, params, function (err, result) {
+                if (err) {
+                    res.status(400).json({ "error": err.message })
+                    return;
+                }
+            });
+            res.json({
+                message: "success",
+                data: data
+            })
+        });
+})
+
+
+app.delete("/api/phone/:phone", md_auth.ensureAuth, (req, res, next) => {
+    db.run(
+        'DELETE FROM phone WHERE phone = ?',
+        req.params.phone,
         function (err, result) {
             if (err) {
                 res.status(400).json({ "error": res.message })
                 return;
             }
+            var sql = 'INSERT INTO log (action, phone, status, time) VALUES (?,?,?,?)'
+            var datetime = new Date();
+            var params = ['Teléfeno eliminado', req.params.phone, "success", datetime.toISOString()]
+            db.run(sql, params, function (err, result) {
+                if (err) {
+                    res.status(400).json({ "error": err.message })
+                    return;
+                }
+            });
             res.json({ "message": "deleted", rows: this.changes })
         });
+})
+
+app.post("/api/login/", (req, res, next) => {
+    var errors = []
+    if (!req.body.password) {
+        errors.push("No password specified");
+    }
+    if (!req.body.email) {
+        errors.push("No email specified");
+    }
+    if (errors.length) {
+        res.status(400).json({ "error": errors.join(",") });
+        return;
+    }
+    var data = {
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    }
+    var sql = `select * from user 
+           WHERE email = ? and password = ?`
+    console.log(sql)
+    var params = [data.email, data.password]
+    db.all(sql, params, (err, rows) => {
+        console.log(rows)
+        if (err) {
+            var sql = 'INSERT INTO log (action, phone, status, time) VALUES (?,?,?,?)'
+            var datetime = new Date();
+            var params = ['Error en login ', data.email, "error", datetime.toISOString()]
+            db.run(sql, params, function (err, result) {
+                if (err) {
+                    res.status(400).json({ "error": err.message })
+                    return;
+                }
+            });
+            res.status(400).json({ "error": err.message });
+            return;
+        }
+        if (rows.length == 0) {
+            res.json({
+                "error": "error"
+            })
+        } else {
+            var sql = 'INSERT INTO log (action, phone, status, time) VALUES (?,?,?,?)'
+            var datetime = new Date();
+            var params = ['Usuario logueado ', data.email, "success", datetime.toISOString()]
+            db.run(sql, params, function (err, result) {
+                if (err) {
+                    res.status(400).json({ "error": err.message })
+                    return;
+                }
+            });
+            rows.password = ""
+            res.json({
+                "token": jwt.createToken(data),
+                "user": rows
+            })
+        }
+    });
 })
 
 
